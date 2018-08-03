6549ba079f142ad45f6fb84abb46cc220b2fcabf
Danny Pifer <piferdg@gmail.com>
2018-08-02 20:14:45 -0600

    Adding migratons and seeds

---
+++ migrations/20180802200526_game.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('game', function(game){
+    game.increments()
+    game.string('name')
+    game.string('developer')
+    game.float('rating')
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('game')  
+}
+
